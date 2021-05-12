9df2c711ad29b7b18f6f689b99b87e828d66152e
Mike Nieva <m@mikenieva.com>
2021-05-12 16:10:12 -0500

    Proyecto actualizado con MongoDBStore

--- configs/session.config.js
+++ configs/session.config.js
@@ -1,31 +1,22 @@
-// configs/session.config.js
 
-// require session
 const session = require('express-session');
-
-// ADDED: require mongostore
-// const MongoStore = require('connect-mongo')(session);
-
-// ADDED: require mongoose
-const mongoose = require('mongoose');
+const MongoDbStore = require('connect-mongo')
 
 module.exports = app => {
+
   app.use(
     session({
       secret: process.env.SESS_SECRET,
       resave: true,
       saveUninitialized: false,
       cookie: {
-        sameSite: true,
+        sameSite: false,
         httpOnly: true,
-        maxAge: 60000
-      }
-    //   store: new MongoStore({
-    //     // <== ADDED !!!
-    //     mongooseConnection: mongoose.connection,
-    //     // ttl => time to live
-    //     ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
-    //   })
+        maxAge: 60000 // 60 * 1000 ms === 1 min
+      },
+      store: MongoDbStore.create({
+        mongoUrl: process.env.MONGODB_URL
+        })
     })
   );
 };
