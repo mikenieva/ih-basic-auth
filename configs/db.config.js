79e5354dbcfb21694eede747f0f0213bc379726b
Mike Nieva <m@mikenieva.com>
2021-05-14 11:32:55 -0500

    Cambios previos

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/basicAuth';
 
 mongoose
-  .connect(MONGODB_URI, {
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
   })
-  .then(() => console.log(`Successfully connected to the database ${MONGODB_URI}`))
+  .then(() => console.log(`Successfully connected to the database`))
   .catch(error => {
-    console.error(`An error ocurred trying to connect to the database ${MONGODB_URI}: `, error);
+    console.error(`An error ocurred trying to connect to the database: `, error);
     process.exit(1);
   });
